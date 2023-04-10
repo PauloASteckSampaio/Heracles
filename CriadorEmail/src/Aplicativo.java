3351c8938a9196a526d61c3f3411c5102ab2ca3a
PauloAntonioSteckSampaio <pauloassampaio2022@gmail.com>
2023-04-10 10:14:52 -0300

    Criação e começo em projeto de criação de emails.

---
+++ CriadorEmail/src/Aplicativo.java
@@ -0,0 +1,8 @@
+public class Aplicativo {
+    // Esta aplicação ira: Criar um emeail seguindo um modelo, determinar o departamento do dono do email, gerar
+    // uma senha, ter metodos para mudar a senha, definir a capacidade da caixa de email, definir endereço de email
+    // alternativo e possui métodos para exibi o nome, email e a capacidade da caixa de email.
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ CriadorEmail/src/Email.java
@@ -0,0 +1,11 @@
+public class Email {
+    private String primeiroNome;
+    private String sobrenome;
+    private String senha;
+    private String departamento;
+    private int caixaDeEmail;
+    private String emailAlternativo;
+
+
+
+}
